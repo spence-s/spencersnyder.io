ca45ef171d713937ce431797ea0817ea0c375fc4
Spencer Snyder <sasnyde2@gmail.com>
2019-03-03 22:51:43 -0500

    fix build

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 import PropTypes from 'prop-types';
-import Layout from '../components/Layout';
+import Layout from '../components/layout';
 
 export default function Template({
   data // this prop will be injected by the GraphQL query below.
